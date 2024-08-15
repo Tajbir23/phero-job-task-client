e02b5d91ded513cd803d3d7492a56b3eb1164037
Tajbir23 <studenttajbirislam@gmail.com>
2024-08-15 18:51:49 +0600

    implement: secure url

---
+++ src/hooks/useSecureUrl.jsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../providers/AuthProvider";
+import axios from "axios";
+
+const secureUrl = axios.create({
+    baseURL: "http://localhost:5000"
+})
+const useSecureUrl = () => {
+    const navigate = useNavigate();
+    const {logOut, setLoading} = useContext(AuthContext);
+
+    secureUrl.interceptors.request.use(
+        function (config) {
+            const token = localStorage.getItem('token');
+            if(token){
+                config.headers.authorization = `Bearer ${token}`;
+            }
+            return config;
+        },
+        function (error) {
+            return Promise.reject(error);
+        }
+    );
+
+    secureUrl.interceptors.response.use(
+        function (response){
+            return response;
+        },
+        async (error) => {
+            const status = error?.response?.status
+            if(status === 401 || status === 403){
+                setLoading(true);
+                await logOut();
+                navigate("/", { replace: true });
+                setLoading(false);
+            }
+            return Promise.reject(error);
+        }
+    );
+    
+    return secureUrl;
+}
+
+export default useSecureUrl;
