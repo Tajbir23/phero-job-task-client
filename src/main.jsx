6e9184f9283f8fe981222fd5121a8e9e7ff7fa09
Tajbir23 <studenttajbirislam@gmail.com>
2024-08-15 15:23:15 +0600

    authentication done

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/router.jsx'
-import AuthProvider from './providers/AuthProvider.jsx'
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
+      <Toaster position="top-right" reverseOrder={false} />
       <RouterProvider router={router} />
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/Authenticaion/SignUp.jsx
+++ src/pages/Authenticaion/SignUp.jsx
@@ -1,10 +1,11 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import baseUrl from "../../hooks/useBaseUrl";
 import axios from "axios";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import toast from "react-hot-toast";
 const SignUp = () => {
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
@@ -47,10 +48,33 @@ const SignUp = () => {
 
     } catch (error) {
       setLoading(false)
+      toast.error(error.message)
       console.log(error)
     }
 
   }
+
+  const handleGoogleLogin = async(e) => {
+		e.preventDefault()
+
+		try {
+			const provider = new GoogleAuthProvider()
+		const result = await signInWithPopup(auth, provider)
+		const user = await result.user
+		const {data} = await baseUrl.post('/signup', {name: user.displayName, email: user.email, photoUrl: user.photoURL})
+		localStorage.setItem('token', data.token)
+		setUser({
+			name: user.displayName,
+			email: user.email,
+            uid: user.uid,
+            photoUrl: user.photoURL,
+		})
+		navigate(from, {replace: true})
+		} catch (error) {
+      toast.error(error.message)
+			console.log(error.message)
+		}
+	}
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800">
@@ -124,6 +148,14 @@ const SignUp = () => {
               </button>}
               
             </div>
+            <div className="my-6 space-y-4">
+		<button aria-label="Login with Google" type="button" onClick={handleGoogleLogin} className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600 hover:bg-blue-900 hover:text-white">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+				<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+			</svg>
+			<p>Login with Google</p>
+		</button>
+	</div>
             <p className="px-6 text-sm text-center dark:text-gray-600">
               Do you have an account?
               <Link
